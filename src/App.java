1e7d4bf6124604cf219cdba3e74376fa93f7fbde
svn <jsesv96@gmail.com>
2023-02-27 15:23:59 -0600

    Agregando mensaje desde windows

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hola mundo desde ubuntu");
+
+        System.out.println("Hola mundo desde windows");
     }
 }
